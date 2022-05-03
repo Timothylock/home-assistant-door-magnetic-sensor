f9d95d8908d141a9d6fac999aa4fd1a704d92df6
Timothy Lock <me@timothylock.ca>
2022-05-03 19:50:55 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,61 @@
+from flask import Flask
+from flask import request
+import json
+app = Flask(__name__)
+
+# Variables
+isOn = False
+
+# App routes
+@app.route("/")
+def index():
+    return "It Works! Try interacting with /state instead"
+
+
+@app.route("/state", methods=['GET', 'POST'])
+def state():
+    if request.method == 'POST':
+        return setState()
+    elif request.method == "GET":
+        return fetchState()
+
+# Helper Functions
+def setState():
+    c = request.json
+
+    if "active" not in c:
+        return "missing \"active\" field", 400
+
+    if c["active"] == "true":
+        if turnOn():
+            return "turned on", 200
+        else:
+            return "failed to turn on", 500
+
+    if c["active"] == "false":
+        if turnOff():
+            return "turned off", 200
+        else:
+            return "failed to turn off", 500
+
+    return "Invalid payload", 400
+
+def fetchState():
+    return json.dumps({"is_active": str(isOn).lower()}), 200
+
+def turnOn():
+    global isOn
+    isOn = True
+
+    # Implement this. Return False on any error
+    return True
+
+def turnOff():
+    global isOn
+    isOn = False
+
+    # Implement this. Return False on any error
+    return True
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
